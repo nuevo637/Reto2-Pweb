e69f000cc9b10ff9ef50e793ccf263d1debe0e12
SteveArms <acunoc@unsa.edu.pe>
2024-07-12 11:00:14 -0500

    Agregando el servicio añadir para las peliculas

--- frontend/src/app/services/movie.service.ts
+++ frontend/src/app/services/movie.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../interfaces/movie.interface';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +18,17 @@ export class MovieService {
     return this.http.get<Movie[]>(this.baseUrl); // Devuelve un Observable del tipo Movie[]
   }
 
-  
+  addMovies(movie: Movie): Observable<Movie>{
+    const url = `${this.baseUrl}add`;
+    const headers = new HttpHeaders({'Content-type' : 'application/json'});
+    return this.http.post<Movie>(url, movie, {headers})
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: any){
+    console.error('Error de solicitud', error)
+    return throwError('Hubo problema en la solicitud');
+  }
 }
