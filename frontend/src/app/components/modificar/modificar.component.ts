adf2dde91cdc01e13e7960f22325158116b5a8c3
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2024-07-12 21:02:36 -0500

    En el componente eliminar-listar, se agrega la función navegarModificacion, la cual obtiene el id de la pelicula y luego redirige al componenete modificar junto con el id

--- frontend/src/app/components/modificar/modificar.component.ts
+++ frontend/src/app/components/modificar/modificar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet, ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modificar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, CommonModule, FormsModule],
   templateUrl: './modificar.component.html',
   styleUrl: './modificar.component.css'
 })
-export class ModificarComponent {
 
+export class ModificarComponent implements OnInit{
+  pelicula: any = {};
+  peliculaID!: number;
+
+  constructor(private movieService: MovieService, private route: ActivatedRoute){
+    this.peliculaID = +this.route.snapshot.paramMap.get('id')!;
+  }
+
+  ngOnInit(): void {
+    this.movieService.getMovieById(this.peliculaID).subscribe(data => {
+      this.pelicula = data;
+    });
+  }
+
+  guardar(): void {
+    this.movieService.updateMovie(this.peliculaID, this.pelicula).subscribe(() => {
+
+    });
+  }
 }
