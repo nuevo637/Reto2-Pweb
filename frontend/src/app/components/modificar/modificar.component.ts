f810bfed8c578ed8c0cb825655a502ced98aaf0b
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2024-07-12 21:17:49 -0500

    OnInit

--- frontend/src/app/components/modificar/modificar.component.ts
+++ frontend/src/app/components/modificar/modificar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet, ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movie.service';
 import { CommonModule } from '@angular/common';
