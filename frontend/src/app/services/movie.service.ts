35c5af82ff7b86d6ccf7aebef4077c21d78858f3
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2024-07-12 20:51:00 -0500

    En los servicios del frontend, se agregan 2 funciones, getMovieById retorna un observable segun el endpoint de nuestra api

--- frontend/src/app/services/movie.service.ts
+++ frontend/src/app/services/movie.service.ts
@@ -15,6 +15,7 @@ export class MovieService {
   getMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(this.baseUrl); // Devuelve un Observable del tipo Movie[]
   }
+
   deleteMovie(movieId: number): Observable<any> {
     const deleteUrl = `${this.baseUrl}${movieId}/`;
     return this.http.delete(deleteUrl);
@@ -26,5 +27,13 @@ export class MovieService {
       'Content-Type': 'application/json'
     }); 
     return this.http.post<any>(url, movie, {headers});
+  }
+
+  getMovieById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}mostrar/${id}/`);
+  }
+  
+  updateMovie(id: number, movie: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}update/${id}/`, movie);
   }
 }
