8fa580a86b39cb1348b787e5485bc7084e872756
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2024-07-12 20:46:43 -0500

    En los urls del backend, se agregan los endpoints a usar para modificar las peliculas, los cuales requieren un id

--- backend/movie_api/urls.py
+++ backend/movie_api/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('movies/', views.datosDePeliculas, name='movie-list'),
     path('movies/<int:id>/', views.eliminarPelicula, name='movie-delete'),
     path('movies/add/', views.addMovie , name='add-movie'),
+    path('movies/update/<int:movie_id>/', views.actualizarPelicula),
+    path('movies/mostrar/<int:movie_id>/', views.peliculaPorID),
+
 ]
